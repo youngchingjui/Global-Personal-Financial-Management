0e501b6512902933bd6cf00bdc56786bd71767d3
youngchingjui <young.chingjui@gmail.com>
2019-05-20 19:09:30 +0800

    Successfully inserted to database Added more documentation
    Playground.py:
    - Added docstrings to many methods
    - Took out datetime formatter when INSERT to database - not needed
    - Updated logic so if duplicate entry is found, INSERT does not execute
    - Fixed conn.commit() statement
    - Added methods:
    -- extract_balances
    -- save_balance_data
    -- extract_transactions
    -- restructure_transactions

--- Playground.py
+++ Playground.py
@@ -19,6 +19,20 @@ filename = "Raw/Alipay/20180419_20190419_ACCLOG.csv"
 
 # Method to connect to AWS MySQL database
 def connect_aws_db():
+  """
+  Connects to our database on AWS
+
+  Args:
+    None
+
+  Returns:
+    conn (Connection): Our connection object to execute SQL statements
+
+  TODO: 
+    Refactor so host, port, dbname, user, and password can be interchanged.
+    Consider, if re-factored, do we need this method?
+  """
+  
   host = "pfm-rdbs-instance.cd5ryppsxnnf.ap-northeast-2.rds.amazonaws.com"
   port = 3306
   dbname = 'pfmdatabase'
@@ -53,17 +67,38 @@ def extract_file(filename):
     return None
 
 def save_to_bucket(data):
-  #TODO
-  # Connect to cloud bucket to store this file
+  """
+  Saves the decoded raw file to a bucket Google Cloud Platform
 
+  args:
+    data (string): Alipay transaction data, including all of the header data. Data should be decoded
+
+  returns:
+    None
+
+  TODO: Connect to cloud bucket to store this file
+  """
+ 
   pass
   
 def get_header_data(data):
-  # Extract header data, including the following:
-  # - Account name
-  # - Datetime of .csv (from - to)
-  # - Download time
+  """
+  Extracts Alipay transaction file header data, including the following:
+    - Account name
+    - Datetime of .csv (from - to)
+    - Download time
 
+  Args:
+    data (string): Raw, decoded Alipay transaction data
+
+  Returns:
+    header_data (dict): Dictionary with the following structure:
+      - account_name: String
+      - date_range: Struct
+          - from: datetime
+          - to: datetime
+      - download_time: datetime
+  """
   # For now, I'll assume all times are in Shanghai time. Keep an eye out if this is not true
   tz = pytz.timezone('Asia/Shanghai')
   time_format = '%Y-%m-%d %H:%M:%S'
@@ -107,13 +142,16 @@ def get_header_data(data):
 
   return header_data
 
-def formatData(data):
-  # Returns Alipay downloaded statements in a readable format, easy for parsing in a table format
+def extract_transactions(data):
+  """
+  Strips out file header data, and returns Alipay transactions in a Pandas DataFrame format
 
-  # Inputs:
-  # Data: A .csv formatted file that is already decoded properly
+  Args:
+    Data (string): All Alipay transactions and corresponding column headers at top. Transactions must be properly coded, and the file header information is stripped out.
 
-  # Returns: A Pandas datatable
+  Returns: 
+    data_pd (DataFrame): A Pandas datatable with all transactions
+  """
   
   look_for_string = '#----------------------------------------收支明细列表----------------------------------------\n'
   formatted_data = data.split(look_for_string)[1]
@@ -130,30 +168,66 @@ def formatData(data):
 
   return data_pd
 
-def saveData(data) :
-  # Saves data into a specified database
+def restructure_transactions(data):
+  """
+  Restructures transaction data in a better way to conduct analysis. Following is done:
+  - Converts 时间 column to datetime format
+  - Creates new column that determines if a transaction is one of the following categories:
+    - 转账 (transfer)
+    - 支付 (payment)
+    - 收款 (deposit)
+    - 在线支付 (online payment)
+  - Extracts vendor name and/or details from 名称 column
+  - Combines 收入 and 支出 columns into a single column. Since Alipay is an asset account (not a liability), debits will be positive and credits will be negative
+
+  """
+
+  return data
+
+
+def saveData(data):
+  """
+  Saves Alipay transaction data into a specified database
   
-  # Inputs:
-  # data: a Pandas datatable
+  Args:
+    data (DataFrame): 
 
-  # Returns: None
+  Returns
+    None
 
-  # TODO: Build this method
+  TODO: 
+    Build out this method
+  """
+
   pass
 
 def save_to_metadata_table(header_data):
-  #TODO
-  # Adds row of header data to database
-  # Checks if this file has already been added before. If so, it does not add the row.
-  # If this is not duplicate, then is_new = True. If it is duplicate, method returns false.
+  """
+  Adds the header_data to our pfmdatabase.alipay_header database.
+  If this file has already been added before, does not add the header_data and returns is_new=False
+  Checks existing rows if ALL of the following match:
+    - account_name
+    - date_range_from
+    - date_range_to
+    - download_time
 
-  # Returns: is_new, indicating whether this file was previously already uploaded
+  Datetime values are converted and saved in database in UTC timezone
+
+  Args:
+    header_data: Data from the header of alipay transaction downloads. Header_data should have the following dictionary structure:
+      - account_name: String
+      - date_range: Struct
+          - from: datetime
+          - to: datetime
+      - download_time: datetime
+
+  Returns: 
+    is_new (bool): indicates whether header_data is already saved in the database
+  """
   
   print("Saving metadata to MySQL db")
   conn = connect_aws_db()
   cur = conn.cursor()
-  datetime_format = '%Y-%m-%d %H:%M:%S'
-  datetime_format_ms = '%Y-%m-%d %H:%M:%S.%f'
   utc = timezone.utc
 
   # Pull out all of the variables from header_data
@@ -167,19 +241,59 @@ def save_to_metadata_table(header_data):
   query = "SELECT * FROM pfmdatabase.alipay_header WHERE account_name=%s AND date_range_from=%s AND date_range_to=%s AND download_time=%s"
 
   # Make sure to save all the dates in UTC format
-  cur.execute(query, (account_name, date_range_from.astimezone(tz=utc).strftime(datetime_format), date_range_to.astimezone(tz=utc).strftime(datetime_format), download_time.astimezone(tz=utc).strftime(datetime_format)))
+  cur.execute(query, (account_name, date_range_from.astimezone(tz=utc), date_range_to.astimezone(tz=utc), download_time.astimezone(tz=utc)))
   rows = cur.fetchall()
   if len(rows) > 0:
+    print("Found duplicate entry")
     is_new = False
 
-  # Save metadata to table, even if it might be duplicate
-  # Record timestamp of when we're uploading this
-  insertStatement = "INSERT INTO pfmdatabase.alipay_header (account_name, date_range_from, date_range_to, download_time, datetime_uploaded) VALUES(%s, %s, %s, %s, %s)"
-  cur.execute(insertStatement, (account_name, date_range_from.astimezone(tz=utc).strftime(datetime_format), date_range_to.astimezone(tz=utc).strftime(datetime_format), download_time.astimezone(tz=utc).strftime(datetime_format), datetime.now().astimezone(tz=utc).strftime(datetime_format_ms)))
-  conn.commit
+  # If similar entry was not found, continue to save this new entry
+  else :
+    # Save metadata to table, even if it might be duplicate
+    # Record timestamp of when we're uploading this
+    insertStatement = "INSERT INTO pfmdatabase.alipay_header (account_name, date_range_from, date_range_to, download_time, datetime_uploaded) VALUES(%s, %s, %s, %s, %s)"
+    cur.execute(insertStatement, (account_name, date_range_from.astimezone(tz=utc), date_range_to.astimezone(tz=utc), download_time.astimezone(tz=utc), datetime.now().astimezone(tz=utc)))
+    
+    conn.commit()
 
   conn.close()
   return is_new
+
+def extract_balances(data):
+  """
+  Extracts point-in-time balance data from Alipay transactions sheet.
+  Not all financial institutions provides this. Alipay does, so we'll save it.
+
+  Args:
+    data (pandas DataFrame): DataFrame containing Alipay transactions. Must include the 账户余额（元）column
+
+  Returns:
+    balances (pandas DataFrame): DataFrame with the following columns:
+      - 时间
+      - 账户余额（元）
+  """
+
+  # TODO: Double check this code works
+  balances = data.columns(['时间', '账户余额（元）'])
+  return balances
+
+def save_balance_data(balances):
+  """
+  Saves our Alipay point-in-time balance data to pfmdatabase.balances database
+
+  Args:
+    balances (DataFrame): a pandas DataFrame containing the following columns: 
+      - 时间
+      - 账户余额（元）
+  
+  Returns:
+    None
+  """
+
+  # Connect to the database
+  conn = connect_aws_db()
+  cur = conn.cursor()
+  utc = timezone.utc
 
 # Get the data from the file
 data = extract_file(filename)
@@ -189,12 +303,22 @@ save_to_bucket(data)
 
 # Get header data from file
 header_data = get_header_data(data)
-print(header_data)
+print("Our header data: {}".format(header_data))
+
 # Save the header_data to metadata table
 is_new = save_to_metadata_table(header_data)
 
-# Extract and structure the data
-data = formatData(data)
+# Extract transactions
+data = extract_transactions(data)
+
+# Restructures the data so that it's more uniform and we can conduct analysis on the data
+data = restructure_transactions(data)
+
+# Extract point-in-time balances from each transaction
+balances = extract_balances(data)
+
+# Save balances in database
+save_balance_data(balances)
 
 # Save the data locally for now. 
 # TODO: Take this out when no longer needed
